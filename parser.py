ad5b182903116501e8920db73cce06b31faf4c11
StrawHatAaron <aaronmiller822028@gmail.com>
2020-12-11 20:43:33 -0800

    hmmm

--- parser.py
+++ parser.py
@@ -1,16 +1,24 @@
+#Aaron Miller and Lang Le CSc 154 Project
 import os 
-#print os.sys.path
 from scapy.all import *
 import textparser
 from textparser import Sequence
 
 pkts_dict=[]
 count = 0
-pkts = rdpcap("main.cap")
+pkts = rdpcap("../smtp.cap")
 
 for pkt in pkts:
     pkts_dict.append({
         'time': (pkt.time/1000),
         #'packetNumber': pkt.show(dump=true).split('\n')[1] ,
         #'size': pkt.show(dump=true).split('/n')
-        'protocol': 1})
+        'protocol': pkt.show(dump=True).split("###[ IP ]###", 1),
+        'id': count
+        })
+    pkt.show()
+    print pkts_dict[count]['protocol']
+    print "\n"
+    count = count + 1
+
+
